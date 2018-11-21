c6b4ec942e8bfa4c523954aed03372f9a8bd9e62
ShayanSolid <shayan.shsh@icloud.com>
2018-11-21 17:45:57 +0100

    - added first overlay in home page. - added some plugins

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,53 +1,69 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link>
-      <router-link to="/gallery">Gallery</router-link>
-      <router-link to="/blog">Blog</router-link>
-      <router-link to="/newsletter">Newsletter</router-link>
+    <div id="app">
+        <div id="nav">
+            <router-link to="/">Home</router-link>
+            <router-link to="/gallery">Gallery</router-link>
+            <router-link to="/blog">Blog</router-link>
+            <router-link to="/newsletter">Newsletter</router-link>
+        </div>
+        <div id="content">
+            <router-view/>
+        </div>
+        <Footer/>
     </div>
-    <div id="content">
-      <router-view/>
-    </div>
-    <Footer/>
-  </div>
 </template>
 
 <style>
-  #app {
-    font-family: 'Avenir', Helvetica, Arial, sans-serif;
-    -webkit-font-smoothing: antialiased;
-    -moz-osx-font-smoothing: grayscale;
-    text-align: center;
-    color: #2c3e50;
-    height: 100%;
-    display: flex;
-    flex-direction: column;
-  }
+    #app {
+        font-family: 'Avenir', Helvetica, Arial, sans-serif;
+        -webkit-font-smoothing: antialiased;
+        -moz-osx-font-smoothing: grayscale;
+        text-align: center;
+        color: #2c3e50;
+        height: 100%;
+        display: flex;
+        flex-direction: column;
+    }
 
-  #nav {
-    padding-top: 20px;
-    display: flex;
-    justify-content: space-around;
-  }
+    #nav {
+        padding-top: 20px;
+        display: flex;
+        justify-content: space-around;
+    }
 
-  #nav a {
-    font-weight: bold;
-    color: #2c3e50;
-  }
+    #nav a {
+        font-weight: bold;
+        color: #2c3e50;
+    }
 
-  #nav a.router-link-exact-active {
-    color: #42b983;
-  }
+    #nav a.router-link-exact-active {
+        color: #42b983;
+    }
 
-  #content {
-    flex-grow: 1;
-  }
+    #content {
+        flex-grow: 1;
+    }
 </style>
 <script>
-  import Footer from "./components/Footer"
+    import Footer from "./components/Footer"
+    import WebFontLoader from 'webfontloader'
 
-  export default {
-    components: {Footer}
-  }
+
+    export default {
+        // Loading fonts asynchronously
+        mounted() {
+            WebFontLoader.load({
+                google: {
+                    families: ['Roboto:100,300,400,500,700,900']
+                },
+                active: this.setFontLoaded
+            })
+        },
+        methods: {
+            setFontLoaded() {
+                this.$emit('font-loaded')
+            }
+        },
+        components: {Footer}
+    }
 </script>
