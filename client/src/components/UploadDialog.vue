cbf4391fd25b0b3a67cef2ecc7dc1dc835317718
ShayanSolid <shayan.shsh@icloud.com>
2018-11-21 19:21:22 +0100

    - added Picture upload overlay to galleryBase

---
+++ client/src/components/UploadDialog.vue
@@ -0,0 +1,47 @@
+<template>
+    <v-layout row justify-center>
+        <v-dialog v-model="dialog" persistent max-width="600px">
+            <v-btn slot="activator" color="primary" dark>Open Dialog</v-btn>
+            <v-card>
+                <v-card-title>
+                    <span class="headline">Picture Uploader</span>
+                </v-card-title>
+                <v-card-text>
+                    <v-container grid-list-md>
+                        <v-layout wrap>
+                            <v-flex xs12>
+                                <v-btn color="blue darken-1" flat @click.native="dialog = false">Choose Picture(s)</v-btn>
+                            </v-flex>
+                            <v-flex xs12>
+                                <v-text-field label="Picture name*" required></v-text-field>
+                            </v-flex>
+                            <v-flex xs12>
+                                <v-autocomplete :items="['Skiing', 'Ice hockey', 'Soccer', 'Basketball', 'Hockey', 'Reading', 'Writing', 'Coding', 'Basejump']"
+                                              label="Tags"
+                                              hint="you can write verious tags divided by a comma in between: sky, city, cars"
+                                              required
+                                              multiple></v-autocomplete>
+                            </v-flex>
+                        </v-layout>
+                    </v-container>
+                    <small>*indicates required field</small>
+                </v-card-text>
+                <v-card-actions>
+                    <v-spacer></v-spacer>
+                    <v-btn color="blue darken-1" flat @click.native="dialog = false">Close</v-btn>
+                    <v-btn color="blue darken-1" flat @click.native="dialog = false">Upload</v-btn>
+                </v-card-actions>
+            </v-card>
+        </v-dialog>
+    </v-layout>
+</template>
+
+<script>
+    export default {
+        data () {
+            return {
+                dialog: false
+            }
+        }
+    }
+</script>
--- client/src/views/BaseGallery.vue
+++ client/src/views/BaseGallery.vue
@@ -1,29 +1,32 @@
 <template>
-  <div>
-    <h1>Gallery</h1>
-    <router-view></router-view>
-  </div>
+    <div>
+        <h1>Gallery</h1>
+        <uploadOl/>
+        <router-view></router-view>
+    </div>
 </template>
 
 <script>
-  import postJson from '../mixins/postJson'
+    import postJson from '../mixins/postJson'
+    import uploadOl from '../components/UploadDialog'
 
-  export default {
-    name: "BaseGallery",
-    beforeCreate() {
-      postJson('/getImageJson', {}).then(json => {
-        this.$store.commit('receiveImages', {images: this.reduceToObject(json.images)})
-      })
-    },
-    methods: {
-      reduceToObject(array) {
-        return array.reduce(function (map, obj) {
-          map[obj._id] = obj
-          return map;
-        }, {})
-      }
+    export default {
+        components: {uploadOl},
+        name: "BaseGallery",
+        beforeCreate() {
+            postJson('/getImageJson', {}).then(json => {
+                this.$store.commit('receiveImages', {images: this.reduceToObject(json.images)})
+            })
+        },
+        methods: {
+            reduceToObject(array) {
+                return array.reduce(function (map, obj) {
+                    map[obj._id] = obj
+                    return map;
+                }, {})
+            }
+        }
     }
-  }
 </script>
 
 <style scoped>
