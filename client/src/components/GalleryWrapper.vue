a11a14bf0ef841e62f07eae1dc3b166d72c15c66
Alexander Heidelberg <s6alheid@uni-bonn.de>
2019-01-31 16:48:21 +0100

    search implement before: and after: filters

--- client/src/components/GalleryWrapper.vue
+++ client/src/components/GalleryWrapper.vue
@@ -65,10 +65,37 @@
             if (term === image.author) {
                 continue
             }
+            if (term.substr(0, 5) === 'user:') {
+                const user = term.substr(5, term.length - 5)
+                if (image.user && image.user.name === user) {
+                    continue
+                }
+            }
+
+            if (term.substr(0, 7) === 'before:') {
+                const date = new Date(term.substr(7, term.length - 7))
+                console.log(date);
+                if (image.createdAt) {
+                    const imageDate = new Date(image.createdAt)
+                    if (imageDate <= date) {
+                        continue
+                    }
+                }
+            }
+
+            if (term.substr(0, 6) === 'after:') {
+                const date = term.substr(6, term.length - 6)
+                if (image.createdAt) {
+                    const imageDate = new Date(image.createdAt)
+                    if (imageDate >= date) {
+                        continue
+                    }
+                }
+            }
             // if (['>', '<'].includes.term.substr(0, 1)) {
-                // const uploadTime = moment(image.uploadTime);
-                // const imageDate = uploadTime.format('YYYY-MM-DD');
-                // const date = term.substr(1, term.length - 1)
+            //     const uploadTime = moment(image.uploadTime);
+            //     const imageDate = uploadTime.format('YYYY-MM-DD');
+            //     const date = term.substr(1, term.length - 1)
             // }
             return false
         }
